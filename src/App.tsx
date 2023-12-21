43e54bb0f3f8a55835dbabb07c07a7ab8b7e0049
RayzaOliveira <rayza.ocr@gmail.com>
2023-12-21 15:40:45 -0300

    feat: update way to show the values on screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,143 @@
-// import "./App.css";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useGoogleMaps, useMap } from "@ttoss/google-maps";
+import { Box, Flex } from "@ttoss/ui";
 import React from "react";
+import ReactJson from "react-json-view";
 
 function App() {
   const { ref, map } = useMap();
   const { status } = useGoogleMaps();
 
+  const [location, setLocation] = React.useState<any>({
+    // Rinconada Library
+    lat: 37.4449739,
+    lng: -122.13914659999998,
+  });
+
+  const [buildingInsights, setBuildingInsights] = React.useState<any>({});
+  const [dataLayers, setDataLayers] = React.useState<any>({});
+
+  const getBuildingInsights = async () => {
+    try {
+      const res = await fetch(
+        `https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude=${
+          location.lat
+        }&location.longitude=${location.lng}&requiredQuality=HIGH&key=${
+          import.meta.env.VITE_GOOGLE_MAPS_API
+        }`
+      );
+
+      const data = await res.json();
+
+      setBuildingInsights(data);
+    } catch (err) {
+      console.log("err::getBuildingInsights::", err);
+    }
+  };
+
+  const getDataLayers = async () => {
+    try {
+      const res = await fetch(
+        `https://solar.googleapis.com/v1/dataLayers:get?location.latitude=${
+          location.lat
+        }&location.longitude=${
+          location.lng
+        }&radiusMeters=100&view=FULL_LAYERS&requiredQuality=HIGH&pixelSizeMeters=0.5&key=${
+          import.meta.env.VITE_GOOGLE_MAPS_API
+        }`
+      );
+      const data = await res.json();
+
+      setDataLayers(data);
+    } catch (err) {
+      console.log("err::getdataLayers::", err);
+    }
+  };
+
+  function prepareGetGeoTiffUrl(geoTiffUrl: string) {
+    if (geoTiffUrl.match("solar.googleapis.com")) {
+      const url = new URL(geoTiffUrl);
+      url.searchParams.set("apiKey", import.meta.env.VITE_GOOGLE_MAPS_API);
+      return url.toString();
+    }
+    return geoTiffUrl;
+  }
+
+  const getDataBasedInUrl = async (baseUrl: string) => {
+    try {
+      const res = await fetch(
+        `${baseUrl}&key=${import.meta.env.VITE_GOOGLE_MAPS_API}`
+      );
+      const data = await res.blob();
+
+      console.log("baseUrl::", baseUrl, { data });
+    } catch (error) {
+      console.log("err::getDataBasedInUrl::", error);
+    }
+  };
+
+  React.useEffect(() => {
+    if (dataLayers.maskUrl) {
+      getDataBasedInUrl(dataLayers.maskUrl);
+      console.log(
+        "prepareGetGeoTiffUrl:::",
+        prepareGetGeoTiffUrl(dataLayers.maskUrl)
+      );
+    }
+  }, [dataLayers]);
+
+  React.useEffect(() => {
+    getBuildingInsights();
+    getDataLayers();
+  }, [location]);
+
   React.useEffect(() => {
     if (status === "ready" && map) {
-      map.setCenter({
-        lat: -26.8912311,
-        lng: -49.0974349,
-      });
+      map.setCenter(location);
 
       map.setZoom(20);
       map.setMapTypeId("satellite");
+
+      map.addListener("dragend", () => {
+        const latLng = {
+          lat: map.getCenter()?.lat(),
+          lng: map.getCenter()?.lng(),
+        };
+
+        setLocation(latLng);
+      });
     }
   }, [status]);
 
   return (
-    <>
-      <div ref={ref} style={{ height: 640, width: 860 }} />
-    </>
+    <Flex
+      sx={{
+        paddingY: "2xl",
+        paddingX: "md",
+        gap: "lg",
+        width: "100%",
+        overflow: "hidden",
+      }}
+    >
+      <Box ref={ref} style={{ height: 640, width: 860 }} />
+
+      <Flex
+        sx={{
+          overflowY: "scroll",
+          flex: "1",
+          wordBreak: "break-all",
+          "& > *": {
+            width: "100%",
+          },
+        }}
+      >
+        <ReactJson
+          collapsed
+          theme={"solarized"}
+          src={{ buildingInsights, dataLayers }}
+        />
+      </Flex>
+    </Flex>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GoogleMapsProvider } from "@ttoss/google-maps";
 import "./index.css";
+import { ThemeProvider } from "@ttoss/ui";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleMapsProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API}>
-      <App />
-    </GoogleMapsProvider>
+    <ThemeProvider>
+      <GoogleMapsProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API}>
+        <App />
+      </GoogleMapsProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
